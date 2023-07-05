fea4f024c44549ef0e3895cbe6e32b6b2c474447
Guilherme Ferreira <guilherme.nmf@gmail.com>
2023-07-05 20:06:42 -0300

    Feat: add print all envs

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return "Hello World!"
+    for k, v in os.environ.items():
+        print(f'{k}={v}')
 
 if __name__ == '__main__':
     port = os.environ.get('FLASK_PORT') or 8080
